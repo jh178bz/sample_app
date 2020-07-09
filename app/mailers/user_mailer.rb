483ff017885f9e9821b7b606c4b420ea74cb5214
Ubuntu <ubuntu@ip-172-31-36-166.us-east-2.compute.internal>
2020-07-09 10:20:20 +0000

    Add password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,14 +10,11 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Account activation"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  
+  # @user.secd_reset_email
+  # UserMailer.password_reset(self).deliver_now
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
 end
